e6297efdf1cd022df1288a605e8f0a2ef91e3190
Edwin Tobias Ariza Tellez <edwint0823@gmail.com>
2024-04-05 11:10:35 -0500

    migrations and database config

---
+++ db/migrate/20240405154650_create_features.rb
@@ -0,0 +1,17 @@
+class CreateFeatures < ActiveRecord::Migration[7.1]
+  def change
+    create_table :features do |t|
+      t.float :mag
+      t.string :place, null: false
+      t.date :time
+      t.text :url, null: false
+      t.boolean :tsunami
+      t.string :mag_type, null: false
+      t.string :title, null: false
+      t.integer :longitude, null: false
+      t.integer :latitude, null: false
+
+      t.timestamps
+    end
+  end
+end
